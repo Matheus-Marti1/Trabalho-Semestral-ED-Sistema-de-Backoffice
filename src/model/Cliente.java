24d2726ff43d1dd0aa009df1d22dc0981752ec4b
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2024-05-31 23:38:50 -0300

    Adicionar classes de cliente

---
+++ src/model/Cliente.java
@@ -0,0 +1,42 @@
+package model;
+
+public abstract class Cliente {
+	private String nome;
+	private String endereco;
+	private int numero;
+	private String complemento;
+	private String cep;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}	
+	
+	
+}
---
+++ src/model/ClienteFisico.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ClienteFisico extends Cliente {
+	private String cpf;
+	private String celular;
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	@Override
+	public String toString() {
+		return cpf + ";" + getNome() + ";" + getEndereco() + ";" + getNumero() + ";" + getComplemento() + ";" + getCep() + ";" + celular;
+	}
+		
+}
